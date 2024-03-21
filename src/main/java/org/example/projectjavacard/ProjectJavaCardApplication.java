75279bbe13e208c8e9f8c7ae765cd724c6df2d48
karamon17 <gevorik18@gmail.com>
2024-03-21 09:18:55 +0400

    create project

---
+++ src/main/java/org/example/projectjavacard/ProjectJavaCardApplication.java
@@ -0,0 +1,13 @@
+package org.example.projectjavacard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectJavaCardApplication.class, args);
+    }
+
+}
